e2eb5e2450bc39945ee061dbd74ac9bc2b20f3c3
Marco <mmarco.molnar@gmail.com>
2024-02-10 23:00:50 +0100

    fixed imports and replaced handles with gizmos.matrix so i can build

--- Assets/Scripts/IK.cs
+++ Assets/Scripts/IK.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
 public class IK : MonoBehaviour
@@ -156,9 +152,9 @@ public class IK : MonoBehaviour
         {
             for (int i = 0; i < chainLength && current != null && current.parent != null; i++) {
                 var scale = Vector3.Distance(current.position, current.parent.position) * 0.1f;
-                Handles.matrix = Matrix4x4.TRS(current.position, Quaternion.FromToRotation(Vector3.up, current.parent.position - current.position), new Vector3(scale, Vector3.Distance(current.parent.position, current.position), scale));
-                Handles.color = Color.green;
-                Handles.DrawWireCube(Vector3.up * 0.5f, Vector3.one);
+                Gizmos.matrix = Matrix4x4.TRS(current.position, Quaternion.FromToRotation(Vector3.up, current.parent.position - current.position), new Vector3(scale, Vector3.Distance(current.parent.position, current.position), scale));
+                Gizmos.color = Color.green;
+                Gizmos.DrawWireCube(Vector3.up * 0.5f, new Vector3(1f, 1f, 1f));
                 current = current.parent;
             }
         }
